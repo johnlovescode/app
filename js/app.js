bb85138f2320ca2516bf780ea49521eadb72aa37
John <john.koven@yahoo.com>
2017-09-18 08:58:07 -0500

    adding files to git; this is the initial commit.

---
+++ js/app.js
@@ -0,0 +1,24 @@
+'use strict';
+angular.module('app', ['ngRoute', '7minWorkout']).
+config(function ($routeProvider,$sceDelegateProvider) {
+    $routeProvider.when('/start', {
+        templateUrl: 'partials/start.html'
+    });
+    $routeProvider.when('/workout', {
+        templateUrl: 'partials/workout.html',
+        controller: 'WorkoutController'
+    });
+    $routeProvider.when('/finish', {
+        templateUrl: 'partials/finish.html'
+    });
+    $routeProvider.otherwise({
+        redirectTo: '/start'
+    });
+    $sceDelegateProvider.resourceUrlWhitelist([
+      // Allow same origin resource loads.
+      'self',
+      'http://*.youtube.com/**']);
+	
+});
+
+angular.module('7minWorkout',[]);
